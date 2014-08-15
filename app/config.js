21939de22bac44702e9b21ad441576e2e1c32e49
mgarbacz <mich.garbacz@gmail.com>
2014-08-15 07:06:31 -0500

    Add default env of production

--- app/config.js
+++ app/config.js
@@ -1,24 +1,26 @@
 var config, env;
 
 env = (function() {
-  switch (window.location.hostname) {
-    case "localhost":
-    case "127.0.0.1":
-      return "development";
-    case "tadbit.michalgarbacz.com":
-      return "production";
-  }
+    switch (window.location.hostname) {
+        case "localhost":
+        case "127.0.0.1":
+            return "development";
+        case "tadbit.michalgarbacz.com":
+            return "production";
+        default:
+            return "production";
+    }
 })();
 
 config = {
-  development: {
-    appRoot: '/',
-    apiUrl: 'http://localhost:8124/cards'
-  },
-  production: {
-    appRoot: '/',
-    apiUrl: 'http://tadbit.herokuapp.com/cards'
-  }
+    development: {
+        appRoot: '/',
+        apiUrl: 'http://localhost:8124/cards'
+    },
+    production: {
+        appRoot: '/',
+        apiUrl: 'http://tadbit.herokuapp.com/cards'
+    }
 };
 
 module.exports = config[env];
